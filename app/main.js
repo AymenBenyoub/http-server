ff8e9d5374f812748e0411c5fa8ff32bc3b7af89
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2024-07-14 21:15:14 +0100

    extract url path

--- app/main.js
+++ app/main.js
@@ -4,18 +4,20 @@ const path = require("path");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const req = data.toString();
-    const reqLine = req.split("\n")[0];
+    const reqLine = data.toString().split("\n")[0];
+    
     const reqPath = reqLine.split(" ")[1];
 
     const filePath = path.join(__dirname, reqPath);
     console.log(reqLine+"\n"+reqPath+"\n"+filePath);
     fs.stat(filePath, (err, stat) => {
-      if (err ) {
-        socket.write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
+      if (err) {
+        const response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+        socket.write(response);
         socket.end();
       } else {
-        socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
+        const response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+        socket.write(response);
         socket.end();
       }
     });
