51e3bbe3e040bc18382ebd867293e6471e259120
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2024-07-14 21:01:54 +0100

    extract url path

--- app/main.js
+++ app/main.js
@@ -1,14 +1,32 @@
 const net = require("net");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-
-
+const fs = require("fs");
+const path = require("path");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n")
+  socket.on("data", (data) => {
+    const req = data.toString();
+    const reqLine = req.split("\n")[0];
+    const reqPath = reqLine.split(" ")[1];
+
+    const filePath = path.join(__dirname, reqPath);
+    console.log(reqLine+"\n"+reqPath+"\n"+filePath);
+    fs.stat(filePath, (err, stat) => {
+      if (err || !stat.isFile()) {
+        socket.write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 Not Found");
+        socket.end();
+      } else {
+        socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
+        socket.end();
+      }
+    });
+  });
+
   socket.on("close", () => {
     socket.end();
+    console.log("Socket closed");
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  console.log("Server is listening on port 4221");
+});
