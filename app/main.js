f53ad69b35559579bd8c0544b13f74416a1728ce
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2024-07-16 15:46:01 +0100

    respond with body

--- app/main.js
+++ app/main.js
@@ -1,30 +1,28 @@
 const net = require("net");
-const fs = require("fs");
-const path = require("path");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const reqLine = data.toString().split("\n")[0];
-    
+    const req = data.toString();
+    const reqLine = req.split("\n")[0];
     const reqPath = reqLine.split(" ")[1];
 
-    const filePath = path.join(__dirname, reqPath);
-    console.log(reqLine+"\n"+reqPath+"\n"+filePath);
-    fs.stat(filePath, (err, stat) => {
-      if (err) {
-        const response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
-        socket.write(response);
-        socket.end();
-      } else {
-        const response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
-        socket.write(response);
-        socket.end();
-      }
-    });
+    if (reqPath === "/") {
+      const okResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n200 OK";
+      socket.write(okResponse);
+    } else if (reqPath.startsWith("/echo/")) {
+      
+      const string = reqPath.split("/")[2];
+      
+      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${string.length}\r\n\r\n${string}`
+      socket.write(response);
+    } else {
+      const notFoundResponse = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 Not Found";
+      socket.write(notFoundResponse);
+    }
+    socket.end();
   });
 
   socket.on("close", () => {
-    socket.end();
     console.log("Socket closed");
   });
 });
