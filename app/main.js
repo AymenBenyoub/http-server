1f5fa5083eb04b0a43b94e0209ae0182d41d6e56
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2024-07-28 23:56:07 +0100

    handle request body

--- app/main.js
+++ app/main.js
@@ -15,21 +15,21 @@ const server = net.createServer((socket) => {
     const req = data.toString();
 
     const reqLine = req.split("\n")[0];
+    const httpMethod = reqLine.split(" ")[0];
     const reqPath = reqLine.split(" ")[1];
 
-    // Extract route
     const route = `/${reqPath.split("/")[1]}`;
 
-    // Define responses
     const okResponse =
       "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n200 OK";
     const notFoundResponse =
       "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 Not Found";
 
-    // Process request
     switch (route) {
       case "/": {
         socket.write(okResponse);
+
+        socket.end();
         break;
       }
       case "/echo": {
@@ -51,39 +51,53 @@ const server = net.createServer((socket) => {
         break;
       }
       case "/files": {
-        const fileName = reqPath.split("/")[2];
+        if (httpMethod === "GET") {
+          const fileName = reqPath.split("/")[2];
 
-        const directory = getOption("--directory");
-        const filePath = path.join(directory, fileName);
-        console.log(filePath);
-        fs.stat(filePath, (err, stats) => {
-          if (err || stats.isDirectory()) {
-            console.error("File not found");
-            socket.write(notFoundResponse);
-            socket.end();
-            return;
-          }
-
-          // const data = fs.readFileSync(filePath);
-          fs.readFile(filePath, (err, data) => {
-            if (err) {
-              console.error("Error reading file");
+          const directory = getOption("--directory");
+          const filePath = path.join(directory, fileName);
+          console.log(filePath);
+          fs.stat(filePath, (err, stats) => {
+            if (err || stats.isDirectory()) {
+              console.error("File not found");
               socket.write(notFoundResponse);
-            } else {
-              const contentType = "application/octet-stream";
-              const responseHeader = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stats.size}\r\n\r\n`;
-
-              socket.write(responseHeader);
-              socket.write(data);
+              socket.end();
+              return;
             }
+
+            // const data = fs.readFileSync(filePath);
+            fs.readFile(filePath, (err, data) => {
+              if (err) {
+                console.error("Error reading file");
+                socket.write(notFoundResponse);
+              } else {
+                const contentType = "application/octet-stream";
+                const responseHeader = `HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${stats.size}\r\n\r\n`;
+
+                socket.write(responseHeader);
+                socket.write(data);
+              }
+              socket.end();
+            });
+          });
+        } else if (httpMethod === "POST") {
+          const fileName = reqPath.split("/")[2];
+          const data = req.split("\r\n\r\n")[1];
+          const directory = getOption("--directory");
+          const filePath = path.join(directory, fileName);
+
+          fs.writeFile(filePath, data, (err) => {
+            if (err) throw err;
+            console.log("File has been created and saved!");
+            socket.write("HTTP/1.1 201 Created\r\n\r\n");
             socket.end();
           });
-        });
-        break;
+          break;
+        }
       }
       default: {
         socket.write(notFoundResponse);
-        socket.end(); // Ensure socket is closed for unmatched routes
+        socket.end();
         break;
       }
     }
@@ -91,7 +105,7 @@ const server = net.createServer((socket) => {
 
   socket.on("error", (err) => {
     console.error("Socket error:", err);
-    socket.destroy(); // Immediately close socket on error
+    socket.destroy();
   });
 
   socket.on("close", () => {
